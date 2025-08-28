dcaa3270f98dcd7bb37875d16e734584d064f247
Zia Ahmed <ziaahmedf17@gmail.com>
2025-08-29 00:28:20 +0500

    Added Colors and Fades on Cards

--- Flashy/CardView.swift
+++ Flashy/CardView.swift
@@ -9,13 +9,26 @@ import SwiftUI
 
 struct CardView: View {
     let card: Card
+    var removal: (() -> Void)? = nil
     
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     @State private var isShowingAnswer = false
+    @State private var offset = CGSize.zero
     
     var body: some View {
         ZStack{
             RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(.white)
+                .fill(
+                    differentiateWithoutColor
+                    ? .white
+                    : .white.opacity(1 - Double(abs(offset.width / 50)))
+                )
+                .background(
+                    differentiateWithoutColor
+                    ? nil
+                    : RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(offset.width > 0 ? .green : .red)
+                )
                 .shadow(radius: 10)
             
             VStack{
@@ -33,6 +46,22 @@ struct CardView: View {
             .multilineTextAlignment(.center)
         }
         .frame(width: 450, height: 250)
+        .rotationEffect(.degrees(Double(offset.width / 5)))
+        .offset(x: offset.width, y: 0)
+        .opacity(2 - Double(abs(offset.width / 50)))
+        .gesture(
+            DragGesture()
+                .onChanged{ gesture in
+                    offset = gesture.translation
+                }
+                .onEnded{ _ in
+                    if abs(offset.width) > 100 {
+                        removal?()
+                    } else {
+                        offset = .zero
+                    }
+                }
+        )
         .onTapGesture {
             isShowingAnswer.toggle()
         }
--- Flashy/ContentView.swift
+++ Flashy/ContentView.swift
@@ -15,19 +15,51 @@ extension View{
 }
 
 struct ContentView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     @State private var cards = Array<Card>(repeating: Card.example, count: 10)
     
     var body: some View {
         ZStack{
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            
             VStack{
                 ZStack{
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: cards[index])
+                        {
+                            withAnimation{
+                                removeCard(at: index)
+                            }
+                        }
                             .stacked(at: index, in: cards.count)
                     }
                 }
             }
+            if differentiateWithoutColor {
+                VStack{
+                    Spacer()
+                    HStack{
+                        Image(systemName: "xmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(.Circle())
+                        Spacer()
+                        Image(systemName: "checkmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(.Circle())
+                    }
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .padding()
+                }
+            }
         }
+    }
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
     }
 }
 
