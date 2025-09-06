c32f147c5223063c8f3b7a74eb4ba126e93b1834
Zia Ahmed <ziaahmedf17@gmail.com>
2025-09-06 17:44:55 +0500

    Count down with timer

--- Flashy/ContentView.swift
+++ Flashy/ContentView.swift
@@ -18,6 +18,12 @@ struct ContentView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     @State private var cards = Array<Card>(repeating: Card.example, count: 10)
     
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    @Environment(\.scenePhase) var scenePhase
+    @State private var isActive = true
+    
     var body: some View {
         ZStack{
             Image("background")
@@ -39,23 +45,42 @@ struct ContentView: View {
             }
             if differentiateWithoutColor {
                 VStack{
+                    Text("Time: \(timeRemaining)")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 5)
+                        .background(.black.opacity(0.75))
+                        .clipShape(Capsule())
+                    
                     Spacer()
                     HStack{
                         Image(systemName: "xmark.circle")
                             .padding()
                             .background(.black.opacity(0.7))
-                            .clipShape(.Circle())
+                            .clipShape(Circle())
                         Spacer()
                         Image(systemName: "checkmark.circle")
                             .padding()
                             .background(.black.opacity(0.7))
-                            .clipShape(.Circle())
+                            .clipShape(Circle())
                     }
                     .foregroundColor(.white)
                     .font(.largeTitle)
                     .padding()
                 }
             }
+        }
+        .onReceive(timer){ time in
+            guard isActive else { return }
+            
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+                
+            }
+        }
+        .onChange(of: scenePhase) { _, newPhase in
+            isActive = (newPhase == .active)
         }
     }
     func removeCard(at index: Int) {
